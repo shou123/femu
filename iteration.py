0d74d7ad6164d2219bb202111e2cf709aa8506da
shiyue <shiyuehou1@gmail.com>
2022-04-30 20:17:41 +0000

    develop execution time

--- iteration.py
+++ iteration.py
@@ -1,31 +1,15 @@
 from ctypes import sizeof
+from datetime import datetime
 
 
 def main():
-    path = "/home/fvm/info/tracefile/IBMObjectStoreTrace026Part0"
-    with open(path) as myfile:
-        head = [next(myfile) for x in range(100)]
-        size = 10
-        for i in range(0, len(head), size):
-            lists = head[i:i+size]
-
-
-
-        # for item in head:
-        #     request_type = item.split(" ")[1].split(".")[1]
-        #     if(request_type == "GET"):
-        #         print("")
-        #     object_id = item.split(" ")[2]
-        #     if request_type !="DELETE":
-        #         size = int(item.split(" ")[3])
-
-def data_slice():
-    path = "/home/fvm/info/tracefile/IBMObjectStoreTrace026Part0"
-    with open(path) as myfile:
-        head = [next(myfile) for x in range(100)]
-        size = 10
-        for i in range(0, len(head), size):
-            lists = head[i:i+size]
+    start_time = datetime.now() 
+    for i in range(10000):
+        print(i)
+    end_time = datetime.now()
+    execution_time  = end_time-start_time
+    print(execution_time.total_seconds())
+    print()
 
 if __name__ == "__main__":
     main()
